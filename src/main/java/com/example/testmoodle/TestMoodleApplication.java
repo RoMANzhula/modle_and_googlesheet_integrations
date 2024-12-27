9b0badaeb9621994aba5a4345cadbbd0b3c98260
RoMANzhula <roman1985al@ukr.net>
2024-12-27 13:40:15 +0200

    Add TestMoodleApplication

---
+++ src/main/java/com/example/testmoodle/TestMoodleApplication.java
@@ -0,0 +1,13 @@
+package com.example.testmoodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMoodleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMoodleApplication.class, args);
+    }
+
+}
