e4be8dcda1e2db1dca5dc2ed178d790341922e10
RoMANzhula <roman1985al@ukr.net>
2024-12-27 20:37:08 +0200

    Up DataFromGoogleSheet - add jpa

--- src/main/java/com/example/testmoodle/components/DataFromGoogleSheet.java
+++ src/main/java/com/example/testmoodle/components/DataFromGoogleSheet.java
@@ -2,9 +2,11 @@ package com.example.testmoodle.components;
 
 
 import com.example.testmoodle.model.GoogleSheetData;
+import com.example.testmoodle.services.GoogleSheetDataService;
 import com.example.testmoodle.services.GoogleSheetsApiService;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.model.ValueRange;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -15,70 +17,56 @@ import java.util.List;
 @Component
 public class DataFromGoogleSheet {
 
+    private final GoogleSheetsApiService googleSheetsApiService;
+    private final GoogleSheetDataService googleSheetDataService;
+    private final String spreadsheetId;
+    private final String range;
+
+    public DataFromGoogleSheet(
+            GoogleSheetsApiService googleSheetsApiService,
+            GoogleSheetDataService googleSheetDataService,
+            @Value("${google.sheets.spreadsheetId}") String spreadsheetId,
+            @Value("${google.sheets.range}") String range
+    ) {
+        this.googleSheetsApiService = googleSheetsApiService;
+        this.googleSheetDataService = googleSheetDataService;
+        this.spreadsheetId = spreadsheetId;
+        this.range = range;
+    }
+
     // Getting data from Google Sheets and initializing objects
-    public static List<GoogleSheetData> getCoursesData() throws GeneralSecurityException, IOException {
-        // Initialize Google Sheets API service
-        Sheets sheetsService = GoogleSheetsApiService.getSheetsService();
+    public void getSpreadsheetDataFromGoogle() throws GeneralSecurityException, IOException {
 
-        // Table ID and range to retrieve data from
-
-        //  https://docs.google.com/spreadsheets/d/HERE_IS_A_spreadsheetId_IN_URL/edit?gid=0#gid=0
-        String spreadsheetId = "1_b-nVtasExample9jYKTHLB34CgoDeT-asQGtm6cABCD";
-
-        // use name of sheet and range for processing
-
-        String range = "Sheet1!A:E";
-            // for one cell
-//        String range = "Sheet1!C3";
-//        String range = "Sheet1!E4";
+        Sheets sheetsService = googleSheetsApiService.getSheetsService();
 
         try {
             // API call to retrieve values from a table
-            ValueRange response = sheetsService.spreadsheets().values()
+            ValueRange response =
+                    sheetsService
+                    .spreadsheets()
+                    .values()
                     .get(spreadsheetId, range)
-                    .execute();
-
-            // Getting values from the answer from one cell
-//            List<List<Object>> values = response.getValues();
-//            if (values != null && !values.isEmpty()) {
-//                for (List<Object> row : values) {
-//                    if (!row.isEmpty()) {
-//                        System.out.printf("Value from the cell: %s\n", row.get(0));
-//                    }
-//                }
-//            } else {
-//                System.out.println("No data to output.");
-//            }
+                    .execute()
+            ;
 
             // Getting values from the answer from the entire (whale) range
             List<List<Object>> values = response.getValues();
             if (values != null && !values.isEmpty()) {
-                for (List<Object> row : values) {
-                    for (Object cell : row) {
-                        System.out.print(cell + "\t");
-                    }
-                    System.out.println(); // break to a new line after each line
-                }
+                System.out.println("Data fetched successfully. Processing...");
+
+                // save data to db
+                googleSheetDataService.processAndSaveData(values);
+
+                System.out.println("Data processed and saved successfully.");
             } else {
-                System.out.println("No data to output.");
+                System.out.println("No data found in the spreadsheet.");
             }
 
         } catch (IOException e) {
+            // TODO: add logger
             e.printStackTrace();
         }
 
-        return null;
-    }
-
-    // Method for processing data
-    private static List<GoogleSheetData> processSaveData(List<List<Object>> values) {
-        // TODO: add processing data (now we get all data to terminal)
-        return null;
-    }
-
-    // Testing - get data from google spreadsheet
-    public static void main(String[] args) throws GeneralSecurityException, IOException {
-        getCoursesData();
     }
 
 }
