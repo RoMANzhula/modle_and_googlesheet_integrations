4e57ee46a8552e59268d38125647c2b684436fae
RoMANzhula <roman1985al@ukr.net>
2024-12-28 23:50:27 +0200

    Up MoodleUserService

--- src/main/java/com/example/testmoodle/services/MoodleUserService.java
+++ src/main/java/com/example/testmoodle/services/MoodleUserService.java
@@ -1,9 +1,13 @@
 package com.example.testmoodle.services;
 
 
+import com.example.testmoodle.dto.UserRegistrationRequest;
+import com.example.testmoodle.model.User;
+import com.example.testmoodle.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
@@ -18,35 +22,34 @@ public class MoodleUserService {
     private String moodleBaseUrl;
 
     private final RestTemplate restTemplate;
+    private final UserRepository userRepository;
 
-    public MoodleUserService(RestTemplate restTemplate) {
+    public MoodleUserService(
+            RestTemplate restTemplate,
+            UserRepository userRepository
+    ) {
         this.restTemplate = restTemplate;
+        this.userRepository = userRepository;
     }
 
 
-    public String createUser(
-            String username,
-            String password,
-            String firstName,
-            String lastName,
-            String email
-    ) {
+    @Transactional
+    public void createUser(UserRegistrationRequest registrationRequest) {
         try {
             // query parameters
             String functionName = "core_user_create_users";
             String url = moodleBaseUrl + "/webservice/rest/server.php" + "?wstoken=" +
-                    token + "&wsfunction=" + functionName
-            ;
+                    token + "&wsfunction=" + functionName;
 
             // form the data for the POST request
             MultiValueMap<String, String> urlParameters = new LinkedMultiValueMap<>();
             urlParameters.add("users[0][createpassword]", "0");
-            urlParameters.add("users[0][username]", username);
+            urlParameters.add("users[0][username]", registrationRequest.getUsername());
             urlParameters.add("users[0][auth]", "manual");
-            urlParameters.add("users[0][password]", password);
-            urlParameters.add("users[0][firstname]", firstName);
-            urlParameters.add("users[0][lastname]", lastName);
-            urlParameters.add("users[0][email]", email);
+            urlParameters.add("users[0][password]", registrationRequest.getPassword());
+            urlParameters.add("users[0][firstname]", registrationRequest.getFirstName());
+            urlParameters.add("users[0][lastname]", registrationRequest.getLastName());
+            urlParameters.add("users[0][email]", registrationRequest.getEmail());
 
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -56,19 +59,23 @@ public class MoodleUserService {
             // send POST-request
             ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, request, String.class);
 
-            String responseBody = response.getBody(); // get the result from the response in String format
+            // save to db
+            User user = new User();
+            user.setUsername(registrationRequest.getUsername());
+            user.setFirstName(registrationRequest.getFirstName());
+            user.setLastName(registrationRequest.getLastName());
+            user.setEmail(registrationRequest.getEmail());
+            user.setPassword(registrationRequest.getPassword());
 
-            // TODO: create save data method
-            System.out.println(responseBody); // temporary processing
+            userRepository.save(user);
 
-            return responseBody;
+            System.out.println("User successfully created in Moodle and saved to the database.");
+
         } catch (Exception e) {
             // TODO: add logger
             e.printStackTrace();
-
+            throw new RuntimeException("Failed to create user in Moodle: " + e.getMessage());
         }
-
-        return null;
     }
 
 }
